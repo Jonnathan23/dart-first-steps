a38e03ff5e244c80c88255e2015a2423b842b1d3
Jonnathan23 <jsaquicela26@gmail.com>
2026-08-14 10:48:44 -0500

    feat: update class 6

--- 06_clases.dart
+++ 06_clases.dart
@@ -1,14 +1,27 @@
 void main() {
-  final Hero wolverine = Hero('Logan', 'Regeneracion');
+  final Hero wolverine = Hero(name: 'Logan', power: 'Regeneracion');
 
   print(wolverine);
+  //print(wolverine.toString());
+  print(wolverine.name);
+  print(wolverine.power);
 }
 
 class Hero {
   String name;
   String power;
 
-  Hero(this.name, this.power);
+  Hero({
+    required this.name,
+    this.power = 'Sin poder'
+  });
+   
+  //Hero(this.name, this.power);
   //Hero(String pName, String pPower) : name = pName, power = pPower; 
+  
+  @override
+  String toString(){
+    return '$name - $power';
+  }
   
 }
